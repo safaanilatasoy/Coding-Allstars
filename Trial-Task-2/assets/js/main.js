c191c843ca7517681ddcb3f47427f224d9c3da50
safaanilatasoy <safaanilatasoy@gmail.com>
2023-08-26 23:00:24 +0300

    main

---
+++ Trial-Task-2/assets/js/main.js
@@ -0,0 +1,46 @@
+// Hamburger Menu
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("menu-open");
+});
+
+
+var TIMEOUT = 6000;
+
+var $radios;
+var $activeRadio;
+var currentIndex;
+var radiosLength;
+
+var interval = setInterval(handleNext, TIMEOUT);
+
+function handleNext() {
+  
+  $radios = $('input[class*="slide-radio"]');
+  $activeRadio = $('input[class*="slide-radio"]:checked');
+
+  currentIndex = $activeRadio.index();
+  radiosLength = $radios.length;
+
+  $radios.attr("checked", false);
+
+  if (currentIndex >= radiosLength - 1) {
+    $radios.first().click();
+  } else {
+    $activeRadio.next('input[class*="slide-radio"]').click();
+  }
+}
+
+function myStopFunction() {
+  clearInterval(interval);
+}
+
+$(".slider").hover(function () {
+  debugger;
+  myStopFunction();
+});
+
+$(".slider").mouseleave(function () {
+  interval = setInterval(handleNext, TIMEOUT);
+});
+
+
